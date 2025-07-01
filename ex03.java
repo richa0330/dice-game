30cda617ca04856ba188dca0683826732699b813
richa0330 <riri2005kano@gmail.com>
2025-07-01 13:47:57 +0900

    Dice game: initial revision

--- ex03.java
+++ ex03.java
@@ -7,13 +7,6 @@ class ex03{
 	Scanner sc = new Scanner(System.in);
 	Random ra = new Random();
 
-	System.out.println("What is your name?");
-	System.out.print("> ");
-
-	String name= sc.nextLine();
-
-	System.out.println("Hello, " + name + "!");
-
 	int die1 = ra.nextInt(6) + 1;
 	int die2 = ra.nextInt(6) + 1;
 	int total = die1 + die2;
@@ -22,13 +15,6 @@ class ex03{
 	System.out.println("Die 1: " + die1);
 	System.out.println("Die 2: " + die2);
 	System.out.println("Total value: " + total);
-
-	if(total>7){
-	    System.out.println(name + " won!");
-	}
-	else{
-	    System.out.println(name + " lost!");
-	}
 	
 	sc.close();
     }
